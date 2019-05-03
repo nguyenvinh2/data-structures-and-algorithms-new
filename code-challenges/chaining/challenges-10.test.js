249140123d7a19be0fe84a7fe001c02ed5518e20
Vinh Nguyen <nguyenv2@outlook.com>
2019-05-03 13:54:02 -0700

    3 done

--- code-challenges/chaining/challenges-10.test.js
+++ code-challenges/chaining/challenges-10.test.js
@@ -45,6 +45,7 @@ For example, [ [0,2,5,4], [2,4,10], [] ] should return [ [1, 32], [1024], [] ].
 
 const divisibleByFiveTwoToThePower = (input) => {
   // Solution code here...
+  return input.map(arr => arr.filter(ele => !(ele%5) && typeof ele === 'number').map(ele => Math.pow(2, ele)));
 };
 
 /* ------------------------------------------------------------------------------------------------
