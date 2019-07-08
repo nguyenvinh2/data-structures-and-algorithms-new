55e05f106060b794fd9eacdd70dd58337d609a61
Vinh Nguyen <nguyenv2@outlook.com>
2019-07-08 16:18:49 -0700

    readme updated

--- challenges/Algorithms/src/main/java/RepeatedWord/RepeatedWord.java
+++ challenges/Algorithms/src/main/java/RepeatedWord/RepeatedWord.java
@@ -3,15 +3,6 @@ package RepeatedWord;
 import DataStructures.Hashtable.Hashtable;
 
 public class RepeatedWord {
-
-    public static void main(String args[]) {
-
-
-        String input = "Once upon a time, there was a brave princess who...";
-        RepeatedWord(input);
-
-    }
-
     public static String RepeatedWord(String input) {
         if (input == null) {
             return null;
@@ -20,11 +11,8 @@ public class RepeatedWord {
         Hashtable wordHash = new Hashtable();
 
         for (int i = 0; i < words.length; i++) {
-            String singleWord = words[i].replaceAll("[^\\w\\s]", "");
-            boolean n = wordHash.contains(singleWord);
-            int key = Hashtable.hash(singleWord);
+            String singleWord = words[i].replaceAll("[^\\w\\s]+$", "");
             if (wordHash.contains(singleWord)) {
-                System.out.println("Match"+words[i]);
                 return singleWord;
             } else {
                 wordHash.add(singleWord, "");
--- challenges/Algorithms/src/test/java/RepeatedWord/RepeatedWordTest.java
+++ challenges/Algorithms/src/test/java/RepeatedWord/RepeatedWordTest.java
@@ -8,8 +8,56 @@ public class RepeatedWordTest {
     @Test
     public void repeatedTest() {
         String input = "Once upon a time, there was a brave princess who...";
-
         assertEquals("a", RepeatedWord.RepeatedWord(input));
+    }
+
+    @Test
+    public void repeatedTestTwo() {
+        String input = "It was the best of times, it was the worst of times, it was the age of wisdom, " +
+                "it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, " +
+                "it was the season of Light, it was the season of Darkness, it was the spring of hope, it was " +
+                "the winter of despair, we had everything before us, we had nothing before us, we were all " +
+                "going direct to Heaven, we were all going direct the other way – in short, the period was " +
+                "so far like the present period, that some of its noisiest authorities insisted on its being " +
+                "received, for good or for evil, in the superlative degree of comparison only...";
+        assertEquals("it", RepeatedWord.RepeatedWord(input));
+    }
+
+    @Test
+    public void repeatedTestThree() {
+        String input = "It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, " +
+                "and I didn’t know what I was doing in New York...";
+        assertEquals("summer", RepeatedWord.RepeatedWord(input));
+    }
+
+    @Test
+    public void repeatedTestPunctuation() {
+        String input = "a!!!! store a??????? store";
+        assertEquals("a", RepeatedWord.RepeatedWord(input));
+    }
+
+    @Test
+    public void repeatedTestEmpty() {
+        String input = "";
+        assertNull(RepeatedWord.RepeatedWord(input));
+    }
+
+    @Test
+    public void noRepeatedWordTest() {
+        String input = "I am Legend";
+        assertNull(RepeatedWord.RepeatedWord(input));
+    }
+
+    @Test
+    public void repeatedWordTestNumbersCount() {
+        String input = "12 I am 12 Legend i";
+        assertEquals("12", RepeatedWord.RepeatedWord(input));
+    }
+
+    @Test
+    public void repeatedWordTestPunctuation() {
+        String input = "I've am Legend ive";
+        assertNull(RepeatedWord.RepeatedWord(input));
     }
 
 }
